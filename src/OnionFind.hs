0fd7cf060493e5f608e677bd0de5d61a61069f50
Jonas Carpay <jonascarpay@gmail.com>
2023-03-01 18:10:28 +0900

    Monad transfomer

--- src/OnionFind.hs
+++ src/OnionFind.hs
@@ -5,14 +5,17 @@
 {-# LANGUAGE RankNTypes #-}
 
 module OnionFind
-  ( IUnify,
+  ( UnifyIT,
+    UnifyT,
     Unify,
+    UnifyI,
     Point,
     fresh,
     unify,
     unifyWith,
     unifyErr,
     unifyGeneric,
+    runUnifyT,
     runUnify,
     writePoint,
     readPoint,
@@ -22,7 +25,6 @@ module OnionFind
     unPoint,
     bind,
     fuse,
-    fuseAp,
     morph,
     morphM,
     morphT,
@@ -31,38 +33,52 @@ module OnionFind
 where
 
 import Control.Monad
+import Control.Monad.Trans.Class
 import Data.Coerce (coerce)
+import Data.Functor.Identity
 import Data.IntMap.Strict
 import qualified Data.IntMap.Strict as IntMap
 
-newtype IUnify s i o a = IUnify
-  { unUnify :: forall r. (a -> IntMap Int -> IntMap o -> Int -> r) -> IntMap Int -> IntMap i -> Int -> r
+-- | Using this as a transformer is technically unsafe.
+-- If the underlying monad can branch, there is no guarantuee that a 'Point' from one brancy doesn't bleed into another, where it is invalid.
+newtype UnifyIT s i o m a = UnifyT
+  { unUnify :: forall r. (a -> IntMap Int -> IntMap o -> Int -> m r) -> IntMap Int -> IntMap i -> Int -> m r
   }
   deriving stock (Functor)
 
-type Unify s t = IUnify s t t
+instance MonadTrans (UnifyIT s t t) where
+  lift m = UnifyT $ \ok l r c -> m >>= (\a -> ok a l r c)
 
-runUnify :: (forall s. IUnify s i o a) -> a
-runUnify (IUnify k) = k (\a _ _ _ -> a) mempty mempty 0
+type UnifyT s t = UnifyIT s t t
 
-instance Applicative (IUnify s t t) where
-  pure a = IUnify $ \ok -> ok a
+type UnifyI s i o = UnifyIT s i o Identity
+
+type Unify s t = UnifyIT s t t Identity
+
+runUnifyT :: Applicative m => (forall s. UnifyIT s i o m a) -> m a
+runUnifyT (UnifyT k) = k (\a _ _ _ -> pure a) mempty mempty 0
+
+runUnify :: (forall s. UnifyI s i o a) -> a
+runUnify k = runIdentity $ runUnifyT k
+
+instance Applicative (UnifyIT s t t m) where
+  pure a = UnifyT $ \ok -> ok a
   (<*>) = ap
 
-instance Monad (IUnify s t t) where
-  IUnify k >>= f = IUnify $ \ok -> k (\a -> (unUnify $ f a) ok)
+instance Monad (UnifyIT s t t m) where
+  UnifyT k >>= f = UnifyT $ \ok -> k (\a -> (unUnify $ f a) ok)
 
-rootElems :: Unify s t [t]
+rootElems :: UnifyT s t m [t]
 rootElems = IntMap.elems <$> rootsRaw
 
-rootKeys :: Unify s t [Point s]
+rootKeys :: UnifyT s t m [Point s]
 rootKeys = coerce . IntMap.keys <$> rootsRaw
 
-roots :: Unify s t [(Point s, t)]
+roots :: UnifyT s t m [(Point s, t)]
 roots = coerce . IntMap.toList <$> rootsRaw
 
-rootsRaw :: Unify s t (IntMap t)
-rootsRaw = IUnify $ \ok l r -> ok r l r
+rootsRaw :: UnifyT s t m (IntMap t)
+rootsRaw = UnifyT $ \ok l r -> ok r l r
 
 find :: Int -> IntMap Int -> (Int, IntMap Int)
 find n l = case IntMap.lookup n l of
@@ -76,52 +92,55 @@ find n l = case IntMap.lookup n l of
 newtype Point s = Point {unPoint :: Int}
   deriving newtype (Eq, Ord, Show)
 
-writePoint :: Point s -> t -> Unify s t ()
-writePoint (Point n) t = IUnify $ \ok l r ->
+writePoint :: Point s -> t -> UnifyT s t m ()
+writePoint (Point n) t = UnifyT $ \ok l r ->
   let (rep, l') = find n l
    in ok () l' (IntMap.insert rep t r)
 
-readPoint :: Point s -> Unify s t t
-readPoint (Point p) = IUnify $ \ok l r c ->
+readPoint :: Point s -> UnifyT s t m t
+readPoint (Point p) = UnifyT $ \ok l r c ->
   let (rep, l') = find p l
    in ok (r IntMap.! rep) l' r c
 
-fresh :: t -> Unify s t (Point s)
-fresh t = IUnify $ \ok link root count -> ok (Point count) link (IntMap.insert count t root) (count + 1)
+{-# INLINE fresh #-}
+fresh :: t -> UnifyT s t m (Point s)
+fresh t = UnifyT $ \ok link root count -> ok (Point count) link (IntMap.insert count t root) (count + 1)
 
-bind :: IUnify s i h a -> (a -> IUnify s h o b) -> IUnify s i o b
-bind (IUnify i) f = IUnify $ \ok l r n -> i (\a l' r' n' -> (unUnify $ f a) ok l' r' n') l r n
+{-# INLINE bind #-}
+bind :: UnifyIT s i h m a -> (a -> UnifyIT s h o m b) -> UnifyIT s i o m b
+bind (UnifyT i) f = UnifyT $ \ok -> i (\a -> (unUnify $ f a) ok)
 
-fuse :: (a -> b -> c) -> IUnify s i h a -> IUnify s h o b -> IUnify s i o c
-fuse f (IUnify i) (IUnify o) = IUnify $ \ok l r n -> i (\a l' r' n' -> o (ok . f a) l' r' n') l r n
+{-# INLINE fuse #-}
+fuse :: (a -> b -> c) -> UnifyIT s i h m a -> UnifyIT s h o m b -> UnifyIT s i o m c
+fuse f (UnifyT i) (UnifyT o) = UnifyT $ \ok -> i (\a -> o (ok . f a))
 
-fuseAp :: IUnify s i h (a -> b) -> IUnify s h o a -> IUnify s i o b
-fuseAp (IUnify i) (IUnify o) = IUnify $ \ok -> i (\f -> o (ok . f))
-
-morph :: (i -> o) -> IUnify s i o ()
+morph :: (i -> o) -> UnifyIT s i o m ()
 morph f = unsafeMorph (fmap f)
 
 {-# ANN morphT "HLint: ignore Eta reduce" #-}
-morphT :: (forall f. Traversable f => f i -> f o) -> IUnify s i o ()
+morphT :: (forall f. Traversable f => f i -> f o) -> UnifyIT s i o m ()
 morphT f = unsafeMorph f
 
-morphM :: (IntMap i -> i -> o) -> IUnify s i o ()
+morphM :: (IntMap i -> i -> o) -> UnifyIT s i o m ()
 morphM f = unsafeMorph (\i -> f i <$> i)
 
-{-# INLINE unsafeMorph #-}
-unsafeMorph :: (IntMap i -> IntMap o) -> IUnify s i o ()
-unsafeMorph f = IUnify $ \ok l r -> ok () l (f r)
+local :: UnifyIT s t t' m () -> UnifyIT s t' t m () -> UnifyT s t' m a -> UnifyT s t m a
+local scope unscope inner = fuse (const id) scope $ fuse const inner unscope
 
-unify :: Point s -> Point s -> Unify s t ()
-unify (Point p1) (Point p2) = IUnify $ \ok l r n ->
+{-# INLINE unsafeMorph #-}
+unsafeMorph :: (IntMap i -> IntMap o) -> UnifyIT s i o m ()
+unsafeMorph f = UnifyT $ \ok l r -> ok () l (f r)
+
+unify :: Point s -> Point s -> UnifyT s t m ()
+unify (Point p1) (Point p2) = UnifyT $ \ok l r n ->
   let (r1, l1) = find p1 l
       (r2, l2) = find p2 l1
    in ok () (IntMap.insert r2 r1 l2) (IntMap.delete r2 r) n
 
-unifyWith :: (t -> t -> t) -> Point s -> Point s -> Unify s t ()
+unifyWith :: (t -> t -> t) -> Point s -> Point s -> UnifyT s t m ()
 unifyWith f p1 p2 = void $ unifyGeneric (\a b -> pure ((), f a b)) p1 p2
 
-unifyErr :: (t -> t -> Either e t) -> Point s -> Point s -> Unify s t (Maybe e)
+unifyErr :: (t -> t -> Either e t) -> Point s -> Point s -> UnifyT s t m (Maybe e)
 unifyErr f p1 p2 = either pure (const Nothing) <$> unifyGeneric f' p1 p2
   where
     f' a b = case f a b of
@@ -129,8 +148,8 @@ unifyErr f p1 p2 = either pure (const Nothing) <$> unifyGeneric f' p1 p2
       Right r -> Right ((), r)
 
 {-# INLINE unifyGeneric #-}
-unifyGeneric :: (t -> t -> Either e (info, t)) -> Point s -> Point s -> Unify s t (Either e info)
-unifyGeneric f (Point p1) (Point p2) = IUnify $ \ok l r n ->
+unifyGeneric :: (t -> t -> Either e (info, t)) -> Point s -> Point s -> UnifyT s t m (Either e info)
+unifyGeneric f (Point p1) (Point p2) = UnifyT $ \ok l r n ->
   let (r1, l1) = find p1 l
       (r2, l2) = find p2 l1
       t1 = r IntMap.! r1
